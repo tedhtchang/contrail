0c9bbae9edc0733183d3eb5febe7f849f5e8c588
tedhtchang <htchang@us.ibm.com>
2015-11-04 16:33:35 -0800

    add contrail yum repo for Openstack Controller node

--- cookbooks/contrail/attributes/default.rb
+++ cookbooks/contrail/attributes/default.rb
@@ -19,6 +19,7 @@ uri = URI("#{Chef::Config[:chef_server_url]}")
 chef_server_ip = uri.host
 default['contrail']['yum_repo_url'] = "https://#{chef_server_ip}:14443/yum-repo/contrail/"
 default['contrail']['yum_compute_repo_url'] = "https://#{chef_server_ip}:14443/yum-repo/contrail-compute/"
+default['contrail']['yum_os_controller_repo_url'] = "https://#{chef_server_ip}:14443/yum-repo/contrail-os-controller/"
 default['contrail']['keystone_ip'] = "#{node['openstack']['endpoints']['host']}"
 default['contrail']['os_controller_ip'] = "#{node['openstack']['endpoints']['host']}" 
 default['contrail']['ct_controller_interface'] = node['network']['default_interface'] 
---
+++ cookbooks/contrail/recipes/yum-os-controller-repo.rb
@@ -0,0 +1,27 @@
+# encoding: UTF-8
+# =================================================================
+# Licensed Materials - Property of IBM
+#
+# (c) Copyright IBM Corp. 2014 All Rights Reserved
+#
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+# =================================================================
+#
+# Cookbook Name:: contrail
+# Recipe:: yum-compute-repo.rb
+#
+
+include_recipe 'yum'
+
+base_contrail_url = node['contrail']['yum_os_controller_repo_url']
+
+yum_repository 'contrail_install' do
+        description 'Contrail os controller repo'
+        baseurl base_contrail_url
+        repositoryid 'Ccontrail_os_controller_repo'
+        gpgcheck false
+        sslverify false
+        action :create
+end
+
