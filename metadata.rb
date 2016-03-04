e227328d557c7d3edca61bf3a73f1df41aaef1a1
Chin Huang <chhuang@us.ibm.com>
2016-03-03 16:54:36 -0800

    Change cookbook version to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'praneetb@juniper.net'
 license          'All rights reserved'
 description      'Installs/Configures contrail'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.9.1'
+version          '1.0.0'
 
 depends 'yum'
 depends 'python'
